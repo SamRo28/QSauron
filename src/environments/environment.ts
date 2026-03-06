a4c08dea9f71f875063517b41f53e5ba4c44a670
SamRo28 <samuel.rodero@alu.uclm.es>
2026-03-06 10:48:17 +0100

    Diagrama de la arquitectura

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
     production: false,
-    quMuUrl: 'https://alarcosj.esi.uclm.es/v2qumuclient',
-    quCoUrl: 'https://alarcosj.esi.uclm.es/v2quco',
-    quTeUrl: 'https://alarcosj.esi.uclm.es/qute',
-    quaCoUrl: 'https://alarcosj.esi.uclm.es/quaco',
+    quMuUrl: 'http://localhost:4200',
+    quCoUrl: 'http://localhost:4201',
+    quTeUrl: 'http://localhost:4202',
+    quaCoUrl: 'http://localhost:4203',
 
     apiUrl: 'http://localhost:8080'
 };
